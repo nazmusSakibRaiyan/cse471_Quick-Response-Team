8d4ef5e6c0854ae27cbd8bec365082d8133e0e1d
Nazmus Sakib Raiyan <nazmus.sakib.raiyan@g.bracu.ac.bd>
2025-05-13 02:26:47 +0600

    change into something

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,10 +1,1 @@
 /// <reference types="vite/client" />       
-
-interface ImportMetaEnv {
-  readonly VITE_REACT_APP_BACKEND_BASEURL: string;
-
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
